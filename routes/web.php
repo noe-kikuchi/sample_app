7930dd650a1b1b4caaf48fe8b8d47961f1c4624c
noe-kikuchi <noenoe888815@gmail.com>
2021-10-15 17:14:20 +0900

    タスク作成機能

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,9 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/index', 'PlayersController@index');
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'FolderController@create');
+
+Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', 'TaskController@create');
